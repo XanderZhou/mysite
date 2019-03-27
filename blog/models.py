5c82b89eda1887d7ffe22a67c8bd5ec991bdfda4
Xander <xander.zhou@gmail.com>
2019-03-28 00:31:36 +0800

    0:31

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,8 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
-
+    class Meta:
+        ordering = ['-created_time']
 
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Blog, BlogType
+
 
 # Create your views here.
 
 def blog_list (request):
+    blogs_all_list = Blog.objects.all()
+    paginator = Paginator(blogs_all_list,10)
+    page_num = request.GET.get('page',1)
+    page_of_blogs = paginator.get_page(page_num)
     context = {}
-    context['blogs'] = Blog.objects.all()
+    context['page_of_blogs'] = page_of_blogs
     context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blog_list.html', context)
 
@@ -20,4 +26,5 @@ def blog_with_type(request,blog_type_pk):
     blog_type = get_object_or_404(BlogType, id=blog_type_pk)
     context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response ('blog/blogs_witch_type.html', context)
